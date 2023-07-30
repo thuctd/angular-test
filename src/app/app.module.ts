7364426e471b325e17697275738fa23dfdc133f7
thuctd <30458438+thuctd@users.noreply.github.com>
2023-07-30 08:31:28 +0700

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { CommonModule } from '@angular/common';
-import { AniamteDirective } from './scroll-directive';
+import { ScrollIntoViewDirective } from './scroll-directive';
 
 @NgModule({
   imports: [
@@ -18,7 +18,7 @@ import { AniamteDirective } from './scroll-directive';
     RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
   ],
   declarations: [AppComponent, TopBarComponent, ProductListComponent,
-    AniamteDirective],
+    ScrollIntoViewDirective],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/scroll-directive.ts
+++ src/app/scroll-directive.ts
@@ -7,10 +7,10 @@ import {
 } from '@angular/core';
 
 @Directive({
-  selector: '[animate]',
+  selector: '[appScrollIntoView]',
 })
-export class AniamteDirective {
-  @Input('animate') myVariable: string;
+export class ScrollIntoViewDirective {
+  @Input('appScrollIntoView') myVariable: string;
   private isElementVisible = false;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
@@ -32,11 +32,11 @@ export class AniamteDirective {
 
       if (isVisible) {
         // Element is scrolled into view
-        console.log(this.myVariable);
+        console.log(this.myVariable)
         this.renderer.addClass(this.elementRef.nativeElement, this.myVariable);
       } else {
         // Element is scrolled out of view
-        console.log(this.myVariable);
+        console.log(this.myVariable)
 
         this.renderer.removeClass(
           this.elementRef.nativeElement,
