5a456f935add8104bb7c6a2d9e196d8561a9eed4
thuctd <30458438+thuctd@users.noreply.github.com>
2024-01-06 14:13:36 +0700

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { jsPDF } from 'jspdf';
-import html2canvas from 'html2canvas';
 
 declare global {
   interface Window {
@@ -25,92 +23,5 @@ callback && callback();
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  // tag: any;
-  player;
-  done: boolean = false;
-  youTubePlayer;
-  videoId: string = 'https://www.youtube.com/watch?v=vwjThI3mMHM';
-
-  convertToPDF() {
-    const element = document.getElementById('test');
-    if (element) {
-      html2canvas(element).then((canvas) => {
-        console.log(canvas);
-        const contentDataURL = canvas.toDataURL('image/png');
-        const pdf = new jsPDF({
-          orientation: 'landscape',
-          unit: 'px',
-          format: [+canvas.height, +canvas.width],
-        });
-        pdf.addImage(
-          contentDataURL,
-          'PNG',
-          0,
-          0,
-          pdf.internal.pageSize.getWidth(),
-          pdf.internal.pageSize.getHeight()
-        );
-        pdf.save('your_file.pdf');
-      });
-    }
-  }
-
-  ngOnInit() {
-    var tag = document.createElement('script');
-    tag.id = 'iframe-demo';
-    tag.src = 'https://www.youtube.com/iframe_api';
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-  }
-
-  onYouTubeIframeAPIReady() {
-    if (!this.player) {
-      this.player = new window.YT.Player('player', {
-        videoId: this.videoId.split('v=')[1],
-        events: {
-          onReady: this.onPlayerReady,
-          onStateChange: this.onPlayerStateChange,
-        },
-      });
-      this.done = true;
-    }
-  }
-
-  detroy() {
-    console.log(this.player);
-    if (!this.player) return;
-    this.player.destroy();
-    this.onYouTubeIframeAPIReady();
-  }
-
-  onPlayerReady(event) {
-    console.log(this.player);
-
-    event.target.playVideo();
-  }
-
-  onPlayerStateChange(event) {
-    if (event.data == window.YT.PlayerState.PLAYING && !this.done) {
-      setTimeout(this.stopVideo, 6000);
-      this.done = true;
-    }
-  }
-
-  stopVideo() {
-    console.log(this.player);
-
-    this.player.stopVideo();
-  }
-
-  playVideo() {
-    this.onYouTubeIframeAPIReady();
-    console.log(this.player);
-    this.player.playVideo();
-  }
-
-  pauseVideo() {
-    console.log(this.player);
-
-    this.player.pauseVideo();
-  }
+  ngOnInit() {}
 }
--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+declare global {
+  interface Window {
+    YT: any;
+  }
+}
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
@@ -8,5 +13,43 @@ import { Component, OnInit } from '@angular/core';
 export class EditorComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  tag = document.createElement('script');
+
+  player = null;
+  done = false;
+  id = '99TJpnnHfxk';
+
+  ngOnInit() {
+    this.tag.src = 'https://www.youtube.com/iframe_api';
+  }
+
+  onYouTubeIframeAPIReady() {
+    this.player = new window.YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: 'M7lc1UVf-VE',
+      playerVars: {
+        playsinline: 1,
+      },
+      events: {
+        onReady: this.onPlayerReady,
+        onStateChange: this.onPlayerStateChange,
+      },
+    });
+  }
+
+  onPlayerReady(event) {
+    event.target.playVideo();
+  }
+
+  onPlayerStateChange(event) {
+    if (event.data == window.YT.PlayerState.PLAYING && !this.done) {
+      setTimeout(this.stopVideo, 6000);
+      this.done = true;
+    }
+  }
+
+  stopVideo() {
+    this.player.stopVideo();
+  }
 }
